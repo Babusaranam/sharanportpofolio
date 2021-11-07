64c8be5aa579346253c3eebf6dd58a7f2a1dd8bb
Babusaranam <babudemat59@gmail.com>
2021-11-07 18:57:56 +0530

    textChanges

--- src/Myoffer.js
+++ src/Myoffer.js
@@ -4,7 +4,7 @@ function Myoffer(){
     return(
         <div className="offer">
                 Welcome to my world . i will create an amazing work experience in the format of work and will provide afriendly atmosphere.
-                <Link type="button" to="Port">push to Port</Link>       
+                <Link type="button"  to="Port">push to Port</Link>       
         </div>
     )
 }
